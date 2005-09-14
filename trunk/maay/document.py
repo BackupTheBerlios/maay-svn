c6b579281e20c908aea5dd0ed6c094d98b630d05
adimasci <adimasci@dcf47c73-9b00-0410-bdd4-e51f759794e1>
2005-09-14 15:51:52 +0000

    changed implementation of document objects to match DB schema and provide useful update/insert/commit functions

--- trunk/maay/document.py
+++ trunk/maay/document.py
@@ -2,18 +2,73 @@
 
 __revision__ = '$Id$'
 
+__all__ = ['Document', 'FileInfo', 'DocumentProvider', 'DocumentScore']
+
 import os
 
-class Document:
+class DBEntity:
+    attributes = []
+    tableName = None
+    key = (None, )
+    
+    def __init__(self, **values):
+        for attrname, value in values.iteritems():
+            assert attrname in self.attributes
+            setattr(self, attrname, value)
+        for keyattr in self.key:
+            assert keyattr in self.attributes, "invalid value for key: %s" % keyattr
 
-    def __init__(self, docId, title, size, text, url="", mimetype="text"):
-        self.title = title
-        self.size = size
-        self.text = text
-        self.docId = docId or makeDocumentId(text)
-        self.url = url
-        self.mimetype = mimetype
+    def buildStateDict(self):
+        return dict([(attr, getattr(self, attr)) for attr in self.attribtutes])
+    stateDict = property(buildStateDict, doc="current object's state")
+    
+    def commit(self, cursor, update=False):
+        if update:
+            query = self.updateQuery()
+        else:
+            query = self.insertQuery()
+        cursor.execute(query, self.stateDict)
+        
 
+    def updateQuery(self):
+        """generates an UPDATE query according to object's state"""
+        attrClauses = ['%s=%%(%s)s' % (attr, attr) for attr in self.attributes
+                       if getattr(self, attr, None)]
+        wheres = ['%s=%%(%s)s' % (keyattr, keyattr) for keyattr in self.key]
+        where = ' AND '.join(wheres)
+        query = 'UPDATE %s SET %s WHERE %s' % (self.tableName, ', '.join(attrClauses),
+                                               where)
+        return query
+
+    def insertQuery(self):
+        """generates an INSERT query according to object's state"""
+        values = ['%%(%s)s' % attr for attr in self.attributes
+                  if getattr(self, attr, None)]
+        query = 'INSERT INTO %s (%s) VALUES (%s)' % (self.tableName,
+                                                     ', '.join(self.attributes),
+                                                     ', '.join(values))
+        return query
+
+    def buildElementsFromTable(cls, cursor):
+        """returns a list of <cls>'s instances based on database content"""
+        query = "SELECT %s FROM %s" % (', '.join(cls.attributes), cls.tableName)
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return [cls(**dict(zip(cls.attributes, row))) for row in results]
+    buildElementsFromTable = classmethod(buildElementsFromTable)
+    
+    def __str__(self):
+        return '%s: %s' % (self.__class__.__name__,
+                           ', '.join(['%s=%s' % (attr, getattr(self, attr))
+                                      for attr in self.attributes]))
+
+class Document(DBEntity):
+    attributes = ('db_document_id', 'document_id', 'mime_type', 'title',
+                  'size', 'text', 'publication_time', 'state', 'download_count',
+                  'url', 'matching', 'index')
+    tableName = 'document'
+    key = ('db_document_id',)
+    
     def readable_size(self):
         bytes = int(self.size)
         if bytes < 1000:
@@ -25,37 +80,32 @@ class Document:
         else:
             return u'%s GB' % (bytes / 10**9)
 
+
     def get_abstract(self):
         return self.text[:200]
     abstract = property(get_abstract)
 
-    def insertQuery(self):
-        """generates an appropriate SQL insert query"""
-        query = """INSERT INTO documents (document_id, mime_type, title, size, text)
-        VALUES (%(docId)s, %(mimetype)s, %(title)s, %(size)s, %(text)s)"""
-        args = dict(self.__dict__)
-        return query, args
-        
+
+class FileInfo(DBEntity):
+    attributes = ('file_name', 'file_time', 'db_document_id',
+                  'state', 'file_state')
+    tableName = 'files'
+    key = ('db_document_id',)
     
+
+class DocumentScore(DBEntity):
+    attributes = ('db_document_id', 'word', 'position', 'download_count',
+                  'relevance', 'popularity')
+    tableName = 'document_scores'
+    key = ('db_document_id',)
+
+class DocumentProvider(DBEntity):
+    attributes = ('db_document_id', 'node_id', 'last_providing_time')
+    tableName = 'document_providers'
+    key = ('db_document_id',)
 
 REMOVED_FILE_STATE = 0
 CREATED_FILE_STATE = 1
 MODIFIED_FILE_STATE = 2
 UNMODIFIED_FILE_STATE = 3
 NOT_INDEXED_FILE_STATE = 4
-
-class FileInfo:
-    def __init__(self, file_name, file_time, db_document_id, state, file_state):
-        self.file_name = file_name
-        self.file_time = file_time
-        self.db_document_id = db_document_id
-        self.state = state
-        self.file_state = file_state
-
-    def __str__(self):
-        return "FileInfo: file_name=%s, file_time=%s, db_document_id=%s, state=%s, file_state=%s" % (self.file_name, self.file_time, self.db_document_id, self.state, self.file_state)
-
-def makeDocumentId(filename):
-    content = file(filename, 'rb').read()
-    return sha.sha(content).hexdigest()
-    
