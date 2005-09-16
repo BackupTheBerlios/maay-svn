f8193a8c7e8d62a926c12def9ebad25954b024aa
afayolle <afayolle@dcf47c73-9b00-0410-bdd4-e51f759794e1>
2005-09-16 14:29:46 +0000

    test auto prop

---
+++ trunk/maay/graphical_indexer.py
@@ -0,0 +1,21 @@
+"""This module contains a Tk wrapper around the indexer, for easier
+operation on Windows boxes
+"""
+
+__revision__ = '$Id$'
+
+from Tkinter import Tk
+
+from maay import indexer
+
+class IndexerGui(Tk):
+
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, **kwargs)
+        self.title("Maay Indexer")
+        self._build_interface()
+        
+
+    def _build_interface(self):
+        for line, (label, info) in enumerate(indexer.IndexerConfiguration.options):
+            pass
