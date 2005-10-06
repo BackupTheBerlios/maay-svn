29cb09b448a8928e9af63f89a600a427b082d829
afayolle <afayolle@dcf47c73-9b00-0410-bdd4-e51f759794e1>
2005-10-06 16:34:27 +0000

    added GPL boilerplate and copyright statement

--- trunk/maay/server.py
+++ trunk/maay/server.py
@@ -1,3 +1,19 @@
+#     Copyright (C) 2005 France Telecom R&D
+#
+#     This program is free software; you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation; either version 2 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program; if not, write to the Free Software
+#     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
 """maay local web UI script"""
 
 __revision__ = '$Id$'
@@ -28,11 +44,15 @@ from twisted.python import failure
 
 from nevow import inevow, rend, tags, guard, loaders, appserver
 from nevow.url import URL
-# this is to help py2exe
+
+# These imports are not used, but they help py2exe tremendously.
+# Do not remove them (that is, unless we change the database backend
+# or drop twisted)
 import nevow.flat.flatstan
 import nevow.query
 import twisted.web.woven.guard
 import MySQLdb
+# end of py2exe helping imports
 
 from logilab.common.textutils import normalize_text
 
