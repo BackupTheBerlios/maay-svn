5ac25dd65e73c09dd87d050499fba985be005fa7
afayolle <afayolle@dcf47c73-9b00-0410-bdd4-e51f759794e1>
2005-11-16 16:53:49 +0000

    moved maay.orig to tags

--- trunk/maay.orig/makedist.bat
+++
@@ -1,4 +0,0 @@
-python setup.py py2exe
-python scripts\nsis.py install dist > install.nsh
-python scripts\nsis.py uninstall dist > uninstall.nsh
-
--- trunk/maay.orig/src/maay/datastructure/nodeinterest.py
+++
@@ -1,7 +0,0 @@
-class NodeInterest:
-    def __init__(self, node_id, word, claim_count, specialisation, expertise):
-        self.node_id = node_id
-        self.word = word
-        self.claim_count = claim_count
-        self.specialisation = specialisation
-        self.expertise = expertise
