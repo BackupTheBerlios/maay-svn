d18a50cac7029ab7831e33fe48619abd79dad779
dnf <dnf@dcf47c73-9b00-0410-bdd4-e51f759794e1>
2005-11-22 15:59:59 +0000

    Update maayservice to display maay icon in border right corner. (early implementation) rem: option --interactive in service argument is used to allow the service to interact with the desktop

--- trunk/maayservice.py
+++ trunk/maayservice.py
@@ -23,7 +23,104 @@ import win32service
 import win32event
 import win32evtlogutil
 
+import win32api
+import win32gui
+import win32con
+import sys, os
+import thread
+import webbrowser
+import maayservice
+
+
 from twisted.internet import reactor
+
+# display small icons on bottom right corner to notify user that maay is running
+# maybe need some refactoring
+
+class Win32MaayTaskbar:
+        SEARCH_MENU_ID = 1001
+        INDEXATION_MENU_ID = 1002
+        HOMEPAGE_MENU_ID = 1003
+        
+	def __init__(self):
+		pass
+
+	def __start__(self):
+		msg_TaskbarRestart = win32api.RegisterWindowMessage("TaskbarCreated");
+		message_map = {
+				msg_TaskbarRestart: self.OnRestart,
+				win32con.WM_DESTROY: self.OnDestroy,
+				win32con.WM_COMMAND: self.OnCommand,
+				win32con.WM_USER+20 : self.OnTaskbarNotify,
+		}
+		# Register the Window class.
+		wc = win32gui.WNDCLASS()
+		hinst = wc.hInstance = win32gui.GetModuleHandle(None)
+		wc.lpszClassName = "MaayTaskbar"
+		wc.style = win32con.CS_VREDRAW | win32con.CS_HREDRAW;
+		wc.hCursor = win32gui.LoadCursor( 0, win32con.IDC_ARROW )
+		wc.hbrBackground = win32con.COLOR_WINDOW
+		wc.lpfnWndProc = message_map # could also specify a wndproc.
+		classAtom = win32gui.RegisterClass(wc)
+		# Create the Window.
+		style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+		self.hwnd = win32gui.CreateWindow( classAtom, "Maay", style, \
+				0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
+				0, 0, hinst, None)
+		win32gui.UpdateWindow(self.hwnd)
+
+		# Try and find a custom icon
+		hinst =  win32gui.GetModuleHandle(None)
+#		iconPathName = os.path.abspath(os.path.join( os.path.split(sys.executable)[0], "maay_node.exe" ))
+
+		icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+		self.hicon = win32gui.LoadImage(hinst, "data\\images\\maay.ico", win32con.IMAGE_ICON, 0, 0, icon_flags)
+
+		flags = win32gui.NIF_ICON | win32gui.NIF_MESSAGE | win32gui.NIF_TIP
+		nid = (self.hwnd, 0, flags, win32con.WM_USER+20, self.hicon, "Maay")
+		win32gui.Shell_NotifyIcon(win32gui.NIM_ADD, nid)
+
+		win32gui.PumpMessages()
+		
+
+	def start(self):
+		thread.start_new_thread(self.__start__, ())
+
+	def OnRestart(self, hwnd, msg, wparam, lparam):
+		self._DoCreateIcons()
+
+	def OnDestroy(self, hwnd, msg, wparam, lparam):
+		nid = (self.hwnd, 0)
+		Shell_NotifyIcon(NIM_DELETE, nid)
+		PostQuitMessage(0) # Terminate the app.
+
+
+	def OnTaskbarNotify(self, hwnd, msg, wparam, lparam):
+		if lparam == win32con.WM_RBUTTONUP:
+			menu = win32gui.CreatePopupMenu()
+			win32gui.AppendMenu( menu, win32con.MF_STRING, Win32MaayTaskbar.SEARCH_MENU_ID, "Search")
+			win32gui.AppendMenu( menu, win32con.MF_STRING, Win32MaayTaskbar.INDEXATION_MENU_ID, "Manage Folders")
+			win32gui.AppendMenu( menu, win32con.MF_SEPARATOR, 1002, "")
+			win32gui.AppendMenu( menu, win32con.MF_STRING, Win32MaayTaskbar.HOMEPAGE_MENU_ID, "Maay Homepage")
+		        win32gui.SetMenuDefaultItem(menu, Win32MaayTaskbar.SEARCH_MENU_ID, 0)
+			pos = win32gui.GetCursorPos()
+			# See http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/menus_0hdi.asp
+			win32gui.SetForegroundWindow(self.hwnd)
+			win32gui.TrackPopupMenu(menu, win32con.TPM_LEFTALIGN, pos[0], pos[1], 0, self.hwnd, None)
+			win32gui.PostMessage(self.hwnd, win32con.WM_NULL, 0, 0)
+		elif lparam==win32con.WM_LBUTTONDBLCLK:
+			webbrowser.open("http://127.0.0.1:7777/", autoraise=1)
+		return 1
+
+	def OnCommand(self, hwnd, msg, wparam, lparam):
+		id = win32api.LOWORD(wparam)
+		if id == Win32MaayTaskbar.SEARCH_MENU_ID:
+			webbrowser.open("http://127.0.0.1:7777/", autoraise=1)
+		elif id == Win32MaayTaskbar.INDEXATION_MENU_ID:
+			webbrowser.open("http://127.0.0.1:7777/indexation", autoraise=1)
+		elif id == Win32MaayTaskbar.HOMEPAGE_MENU_ID:
+			webbrowser.open("http://maay.netofpeers.net", autoraise=1)
+
 
 class MyService(win32serviceutil.ServiceFramework):
     _svc_name_ = "Maay"
@@ -44,7 +141,8 @@ class MyService(win32serviceutil.ServiceFramework):
                                     0, # category
                                     servicemanager.EVENTLOG_INFORMATION_TYPE,
                                     (self._svc_name_, ''))
-
+	taskbar = Win32MaayTaskbar()
+	taskbar.start()
         run()
         # and write a 'stopped' event to the event log.
         win32evtlogutil.ReportEvent(self._svc_name_,
